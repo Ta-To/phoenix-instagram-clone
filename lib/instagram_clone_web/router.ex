58739b56db287886193ca3750cedf0033c7aeece
Ta-To <61908291+Ta-To@users.noreply.github.com>
2022-01-07 14:13:34 +0900

    Modify pages LiveViewを使用する形に変更とタイトル表示まで

--- lib/instagram_clone_web/router.ex
+++ lib/instagram_clone_web/router.ex
@@ -17,7 +17,7 @@ defmodule InstagramCloneWeb.Router do
   scope "/", InstagramCloneWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
+    live "/", PageLive
   end
 
   # Other scopes may use custom stacks.
